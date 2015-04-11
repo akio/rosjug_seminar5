9bc900dd25ac5e059d89d50c6e227c8328ca39d3
Aki Ochiai <akio19937@gmail.com>
2015-04-11 11:40:38 +0900

    add latch option

--- exercise.py
+++ exercise.py
@@ -3,21 +3,24 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 
+def current_goal_callback(msg):
+    pass
+
 rospy.init_node('excesize')
 
-goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1, latch=True)
 
 goal = PoseStamped()
-goal.header.seq = 3
+goal.header.seq = 0
 goal.header.frame_id = 'world'
-goal.header.stamp = rospy.Time.now()
+goal.header.stamp = rospy.Time.now() #+ rospy.Duration(5, 0)
 goal.pose.position.x = 3.5
 goal.pose.position.y = 3.5
 goal.pose.position.z = 0.0
-goal.pose.orientation.w = 1.0
 goal.pose.orientation.x = 0.0
 goal.pose.orientation.y = 0.0
 goal.pose.orientation.z = 0.0
+goal.pose.orientation.w = 1.0
 
 goal_pub.publish(goal)
 

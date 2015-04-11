ca15fec18edd675d3b986420464fe625ab16c663
Aki Ochiai <akio19937@gmail.com>
2015-04-11 12:53:46 +0900

    Cleanup

--- exercise.py
+++ exercise.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
+# vim: fileencoding=utf-8
 
+# rospy
 import rospy
-from geometry_msgs.msg import PoseStamped
+import math
+from geometry_msgs.msg import Quaternion, PoseStamped
+from tf.transformations import quaternion_from_euler
 
-def current_goal_callback(msg):
-    pass
-
+# 
 rospy.init_node('excesize')
 
-goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1, latch=True)
+#
+goal_pub = rospy.Publisher('/move_base_simple/goal',
+                           PoseStamped,
+                           queue_size=1,
+                           latch=True)
 
 goal = PoseStamped()
-goal.header.seq = 0
 goal.header.frame_id = 'world'
-goal.header.stamp = rospy.Time.now() #+ rospy.Duration(5, 0)
+goal.header.stamp = rospy.Time.now()
 goal.pose.position.x = 3.5
 goal.pose.position.y = 3.5
 goal.pose.position.z = 0.0
-goal.pose.orientation.x = 0.0
-goal.pose.orientation.y = 0.0
-goal.pose.orientation.z = 0.0
-goal.pose.orientation.w = 1.0
+q = quaternion_from_euler(0, 0, math.radians(90))
+goal.pose.orientation = Quaternion(*q)
 
 goal_pub.publish(goal)
 
